7f6de8cd6e80acc84963e5a4974ad9169a1433aa
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-02-06 20:17:25 +0545

    fixed private chat view

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -50,15 +50,13 @@ def get_or_create_chatroom(request, username):
     
     other_user = User.objects.get(username=username)
     my_chatrooms = request.user.chat_groups.filter(is_private=True)
-
-    if my_chatrooms.exists():
-        for chatroom in my_chatrooms:
-            if other_user is chatroom.members.all():
-                chatroom = chatroom
-                break
-            else:
-                chatroom = ChatGroup.objects.create(is_private=True)
-                chatroom.members.add(other_user, request.user)
+    
+    chatroom = None
+    for chatroom in my_chatrooms:
+        if other_user in chatroom.members.all():
+            chatroom = chatroom 
+            break
+           
     else:
         chatroom = ChatGroup.objects.create(is_private=True)
         chatroom.members.add(other_user, request.user)
