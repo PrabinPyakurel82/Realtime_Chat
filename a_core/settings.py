9a65065061a7a918af41982b85633633b963ce37
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-02-06 18:15:19 +0545

    redis channels integration

--- a_core/settings.py
+++ a_core/settings.py
@@ -93,11 +93,14 @@ TEMPLATES = [
 #WSGI_APPLICATION = 'a_core.wsgi.application'
 ASGI_APPLICATION = 'a_core.asgi.application'
 
-CHANNEL_LAYERS = {
-    'default' : {
-        "BACKEND" : "channels.layers.InMemoryChannelLayer",
-    }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
 }
 
 # Database
