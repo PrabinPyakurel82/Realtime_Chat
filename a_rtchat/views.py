93e49236b7897814c0bb1fc5df2cfa042021c057
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-02-08 16:14:16 +0545

    leave group functionality

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -118,6 +118,7 @@ def chatroom_edit_view(request,chatroom_name):
     }
     return render(request,'a_rtchat/chatroom_edit.html',context)
 
+@login_required
 def chatroom_delete_view(request,chatroom_name):
     chat_group = get_object_or_404(ChatGroup,group_name=chatroom_name)
     if request.user != chat_group.admin:
@@ -128,3 +129,15 @@ def chatroom_delete_view(request,chatroom_name):
         messages.success(request,"Chatroom deleted successfully")
         return redirect("home")
     return render(request,'a_rtchat/chatroom_delete.html',{'chat_group':chat_group})
+
+@login_required
+def chatroom_leave_view(request,chatroom_name):
+    chat_group = get_object_or_404(ChatGroup,group_name=chatroom_name)
+    if request.user not in chat_group.members.all():
+        raise Http404
+    
+    if request.method == "POST":
+        chat_group.members.remove(request.user)
+        messages.success(request,'You left the chat')
+        return redirect("home")
+    
